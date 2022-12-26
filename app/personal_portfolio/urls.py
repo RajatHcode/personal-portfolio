26ea284c1b3a41e8c5ad3bc31c4590acbec58526
gurdeep.singh <gurdeep.singh@hcode.tech>
2022-12-26 16:53:45 +0530

    test

--- app/personal_portfolio/urls.py
+++ app/personal_portfolio/urls.py
@@ -11,16 +11,13 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add aia URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import json
 
 from django.contrib import admin
 from django.urls import path
-
-# testing pre commit hook
-
-
+import json
 urlpatterns = [
-    path("admin/", admin.site.urls),
+path("admin/", admin.site.urls),
 ]
